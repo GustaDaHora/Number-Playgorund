d1ceb12f8516d8ce651a9a25bfb2eca4ce8c155c
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-07-17 12:26:14 -0400

    Feat: added config file
    styled-components.d.ts
     added to fix the issue with theme

---
+++ src/styled-components.d.ts
@@ -0,0 +1,16 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    COLORS: {
+      BACKGROUND_900: string;
+      BACKGROUND_800: string;
+      BACKGROUND_700: string;
+      WHITE: string;
+      GREEN_100: string;
+      GREEN_200: string;
+      GREEN_300: string;
+      RED: string;
+    };
+  }
+}
