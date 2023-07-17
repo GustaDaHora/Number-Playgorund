c9185e0d531563b6914a71ce3f13a49cc74ecbe0
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-07-17 12:28:48 -0400

    Fix: html root
    Html root was not being used

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import theme from './app/styles/theme';
 import GlobalStyles from './app/styles/global';
 import { ThemeProvider } from 'styled-components';
 import { Routes } from './app/app';
 
-const rootElement = document.createElement('div');
-document.body.appendChild(rootElement);
+const rootElement = document.getElementById('root');
 
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Routes />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
