f6eaa645b846d54acc0d7d8645d0f1fff7146bdc
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-07-18 20:09:21 -0400

    Update: routes
    Added routing to main page

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 
 import { Home } from '../pages/Home';
+import { Main } from '../pages/Main';
 
-export function AppRoutes(){
-    return(
-        <Routes>
-            <Route path='/' element={<Home />} />
-        </Routes>
-    )
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="main" element={<Main />} />
+    </Routes>
+  );
 }
 
 export default AppRoutes;
