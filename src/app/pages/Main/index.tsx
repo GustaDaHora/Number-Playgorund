56fb5ae6ea61f0c59edc298d4998193c88bdfe06
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-07-19 20:48:34 -0400

    Update: Main page
    Added button component to main page

--- src/app/pages/Main/index.tsx
+++ src/app/pages/Main/index.tsx
@@ -1,33 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { Button } from '../../components/Button';
+
 // Styled components for the main page
 const Container = styled.div`
+  width: 100%;
+  height: 100vh;
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
-  background-color: #f2f2f2;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+  > div {
+    display: flex;
+    width: 55%;
+  }
 `;
 
-const Title = styled.h1`
-  font-size: 24px;
-  color: #333;
-  margin-bottom: 16px;
-`;
-
-const Subtitle = styled.p`
-  font-size: 18px;
-  color: #777;
-`;
-
-// Main page component
 export function Main(): React.ReactElement {
   return (
     <Container>
-      <Title>Welcome to My React App</Title>
-      <Subtitle>This is the main page.</Subtitle>
+      <div>
+        {' '}
+        <Button title="Click Me" />
+        <Button title="Click Me" />
+        <Button title="Click Me" />
+      </div>
     </Container>
   );
 }
