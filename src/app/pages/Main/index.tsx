524f9943b5dfe695e7a6d63b267fa4fb1f4ed15d
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-07-20 21:19:42 -0400

    Update tables
    added routes to tables

--- src/app/pages/Main/index.tsx
+++ src/app/pages/Main/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { Button } from '../../components/Button';
+import { Link } from 'react-router-dom';
 
 // Styled components for the main page
 const Container = styled.div`
@@ -17,15 +18,24 @@ const Container = styled.div`
     width: 55%;
   }
 `;
+const LinkButton = styled(Link)`
+  width: 100%;
+
+  height: 56px;
+  border: 3px solid ${({ theme }) => theme.COLORS.WHITE};
+  border-radius: 10px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+`;
 
 export function Main(): React.ReactElement {
   return (
     <Container>
       <div>
         {' '}
-        <Button title="Click Me" />
-        <Button title="Click Me" />
-        <Button title="Click Me" />
+        <LinkButton to="/utilities">Utilidades</LinkButton>
+        <LinkButton title="Em breve" to="" />
+        <LinkButton title="Em breve" to="" />
       </div>
     </Container>
   );
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -2,12 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import { Home } from '../pages/Home';
 import { Main } from '../pages/Main';
+import { Utilities } from '../pages/utilities';
+import { Tables } from '../pages/Tables';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="main" element={<Main />} />
+      <Route path="/main" element={<Main />} />
+      <Route path="/utilities" element={<Utilities />} />
+      <Route path="/tables" element={<Tables />} />
     </Routes>
   );
 }
