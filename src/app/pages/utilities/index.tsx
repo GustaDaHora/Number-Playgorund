26f3c97199c21dc6c2b82b66c05941918c2cb1b6
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-07-27 21:16:25 -0400

    Feat utilities
    added content to utilities page

--- src/app/pages/utilities/index.tsx
+++ src/app/pages/utilities/index.tsx
@@ -3,15 +3,40 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 // Styled components for the main page
-const Container = styled(Link)`
+const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+  > div {
+    display: flex;
+    width: 55%;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+  }
+`;
+const LinkButton = styled(Link)`
+  border: 3px solid ${({ theme }) => theme.COLORS.WHITE};
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  padding: 1rem;
+  font-size: 2rem;
+  transition: 1s;
+
+  &:hover {
+    color: red;
+    padding: 1.5rem;
+    transition: 1s;
+  }
 `;
 
 export function Utilities(): React.ReactElement {
-  return <Container title="Tabuada" to="/tables"></Container>;
+  return (
+    <Container>
+      <LinkButton to="/tables">Tabuada</LinkButton>
+    </Container>
+  );
 }
