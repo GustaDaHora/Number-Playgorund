2fa8ab2a62eb7b11de50f62f3fc0bb2dbea7fbb7
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-08-01 21:02:20 -0400

    Update: cronics
    Styled cronics page

--- src/app/pages/Cronics/index.tsx
+++ src/app/pages/Cronics/index.tsx
@@ -1,22 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import LinkButton from 'src/app/components/Link';
 import { Header } from 'src/app/components/Header';
 
 const Container = styled.div`
   width: 100%;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  min-height: 100vh;
+  box-sizing: border-box;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  justify-content: space-between;
+
+  display: grid;
+  place-items: center;
+  grid-template-rows: 80px 128px auto 64px;
+  grid-template-areas:
+    'header header'
+    'content content';
+
+  > Header {
+    grid-area: header;
+  }
+
+  > div {
+    grid-area: content;
+    display: flex;
+    justify-content: space-between;
+    gap: 1rem;
+  }
 `;
 
 export function Cronics(): React.ReactElement {
   return (
     <Container>
       <Header />
-      Nada aqui
+      <div>
+        {' '}
+        <LinkButton to="/">Em Breve</LinkButton>
+      </div>
     </Container>
   );
 }
