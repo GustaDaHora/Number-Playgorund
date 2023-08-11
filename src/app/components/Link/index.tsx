9b4a25f79ed07adaffb2c15e270f724236f8d531
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-08-10 22:14:04 -0400

    Update index.tsx
    Removed futile comments

--- src/app/components/Link/index.tsx
+++ src/app/components/Link/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-// Create a styled component for the LinkButton
 const LinkButton = styled(Link)`
   border: 3px solid ${({ theme }) => theme.COLORS.WHITE};
   color: ${({ theme }) => theme.COLORS.WHITE};
@@ -10,22 +9,17 @@ const LinkButton = styled(Link)`
   font-size: 2rem;
   transition: 1s;
 
-  /* Styles for hover effect */
   &:hover {
     color: red;
     padding: 1.5rem;
     transition: 1s;
   }
 
-  /* Media query for smaller screens */
   @media (max-width: 768px) {
-    /* Adjust the font size for smaller screens */
     font-size: 1.5rem;
 
-    /* Adjust the padding for smaller screens */
     padding: 0.8rem;
 
-    /* Adjust the hover padding for smaller screens */
     &:hover {
       padding: 1rem;
     }
