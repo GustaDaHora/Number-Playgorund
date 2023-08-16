e833b2ad85149b6b3be0969049e257ed5c626170
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-08-15 22:11:23 -0400

    Update theme colors

--- src/app/styles/theme.ts
+++ src/app/styles/theme.ts
@@ -1,10 +1,10 @@
 export default {
   COLORS: {
-    BACKGROUND_900: '#000800',
+    BACKGROUND_900: '#0d100d',
     BACKGROUND_800: '#001A12',
     BACKGROUND_700: '#00261C',
 
-    WHITE: '#97ED8A',
+    WHITE: '#c2fbd7',
 
     GREEN_100: '##45BF55',
     GREEN_200: '#168039',
