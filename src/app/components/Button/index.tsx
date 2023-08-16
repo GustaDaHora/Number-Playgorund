a3f67e143c047e03a8218b6abf3ac597dbb2d42f
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-08-15 22:09:06 -0400

    Update Button component
    Changed the button style

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -2,27 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Button = styled.button`
-  border: 3px solid ${({ theme }) => theme.COLORS.WHITE};
-  color: ${({ theme }) => theme.COLORS.WHITE};
-  padding: 1rem;
-  font-size: 2rem;
-  transition: 1s;
-  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+  border-radius: 100px;
+  box-shadow: rgba(44, 187, 99, 0.2) 0 -25px 18px -14px inset,
+    rgba(44, 187, 99, 0.15) 0 1px 2px, rgba(44, 187, 99, 0.15) 0 2px 4px,
+    rgba(44, 187, 99, 0.15) 0 4px 8px, rgba(44, 187, 99, 0.15) 0 8px 16px,
+    rgba(44, 187, 99, 0.15) 0 16px 32px;
+  color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  cursor: pointer;
+  display: inline-block;
+  padding: 7px 20px;
+  text-align: center;
+  text-decoration: none;
+  transition: all 250ms;
+  border: 0;
+  font-size: 18px;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
 
   &:hover {
-    color: red;
-    padding: 1.5rem;
-    transition: 1s;
+    box-shadow: rgba(44, 187, 99, 0.35) 0 -25px 18px -14px inset,
+      rgba(44, 187, 99, 0.25) 0 1px 2px, rgba(44, 187, 99, 0.25) 0 2px 4px,
+      rgba(44, 187, 99, 0.25) 0 4px 8px, rgba(44, 187, 99, 0.25) 0 8px 16px,
+      rgba(44, 187, 99, 0.25) 0 16px 32px;
+    transform: scale(1.05) rotate(-1deg);
   }
 
   @media (max-width: 768px) {
-    font-size: 1.5rem;
-
-    padding: 0.8rem;
-
-    &:hover {
-      padding: 1rem;
-    }
+    font-size: 17px;
+    padding: 6px 18px;
   }
 `;
 
