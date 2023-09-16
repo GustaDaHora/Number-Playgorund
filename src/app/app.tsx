0409376438419d3799c1bd1f1c7fa63b4a04b08a
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-09-15 20:59:34 -0400

    Breaking change app.tsx
    Moveed routes to app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,25 @@
-import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-import AppRoutes from './routes';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Main from './pages/Main';
+import Utilities from './pages/Utilities';
+import Tables from './pages/Tables';
+import Cronics from './pages/Cronics';
+import Check from './pages/Check';
 
-export function Routes() {
+const AppRoutes = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/main" element={<Main />} />
+      <Route path="/utilities" element={<Utilities />} />
+      <Route path="/tables" element={<Tables />} />
+      <Route path="/cronics" element={<Cronics />} />
+      <Route path="/check" element={<Check />} />
+    </Routes>
   );
-}
+};
+
+export default AppRoutes;
