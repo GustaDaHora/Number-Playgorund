25b26e0b99d8583b3bf4a47949063cca45d641d8
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-09-15 21:02:56 -0400

    Breaking change: Home
    Removed Three.Js and its canvas

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -1,98 +1,42 @@
-import React, { useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import * as THREE from 'three';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Canvas = styled.canvas`
-  position: fixed;
-  top: 0;
-  left: 0;
+// Styled-components styles
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 `;
 
-export function Home() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const navigate = useNavigate();
+const Button = styled.button`
+  padding: 10px 20px;
+  font-size: 1.2rem;
+  margin: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  background-color: #007bff;
+  color: #fff;
 
-  useEffect(() => {
-    let camera: THREE.PerspectiveCamera;
-    let renderer: THREE.WebGLRenderer;
-    let scene: THREE.Scene;
-    let box: THREE.Mesh;
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
 
-    const init = () => {
-      scene = new THREE.Scene();
-      camera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        1000
-      );
-      renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current! });
-      renderer.setPixelRatio(window.devicePixelRatio);
-      renderer.setSize(window.innerWidth, window.innerHeight);
-
-      camera.position.setZ(50);
-
-      const ambl = new THREE.AmbientLight(0x97ed8a);
-      scene.add(ambl);
-
-      box = new THREE.Mesh(
-        new THREE.BoxGeometry(4, 4, 4),
-        new THREE.MeshBasicMaterial({ color: 0x97ed8a })
-      );
-
-      box.scale.set(3, 3, 3);
-      scene.add(box);
-
-      animate();
-    };
-
-    const animate = () => {
-      requestAnimationFrame(animate);
-
-      box.rotation.y += 0.01;
-      box.rotation.x += 0.01;
-      box.rotation.z += 0.01;
-
-      renderer.render(scene, camera);
-    };
-
-    const handleResize = () => {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-
-      renderer.setSize(window.innerWidth, window.innerHeight);
-    };
-
-    const handleMouseMove = (ev: MouseEvent) => {
-      let changeX = ev.x - oldX;
-      let changeY = ev.y - oldY;
-      camera.position.x += changeX / 90;
-      camera.position.y -= changeY / 90;
-
-      oldX = ev.x;
-      oldY = ev.y;
-    };
-
-    let oldX = 0;
-    let oldY = 0;
-
-    init();
-
-    window.addEventListener('resize', handleResize);
-    window.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
-
-  const handleClick = () => {
-    navigate('/main');
-  };
-
-  return <Canvas onClick={handleClick} ref={canvasRef} id="bg" />;
-}
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Link to="/main">
+        <Button>Main Page</Button>
+      </Link>
+      <Link to="/signup">
+        <Button>Sign Up</Button>
+      </Link>
+    </Container>
+  );
+};
 
 export default Home;
