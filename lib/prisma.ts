ebd97c4a739aef9b328b3c6fb96f150a9213c0bf
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-09-17 21:07:04 -0400

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient | null = null;
+declare global {
+  var prisma: PrismaClient;
+}
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-  if (!prisma) {
-    prisma = new PrismaClient();
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
   }
+  prisma = global.prisma;
 }
 
 export default prisma;
