0465f7e7b96e792a2a7675f1f58dccc331a31429
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-09-24 21:50:12 -0400

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,5 @@
+import { PrismaClient } from '@prisma/client';
+
 declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: import('@prisma/client').PrismaClient;
-    }
-  }
+  var prisma: PrismaClient;
 }
